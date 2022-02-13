30205a03c1f8fc3b3ddbd2ee87bf19eb19f80fb7
ericversteeg <ewversteeg@gmail.com>
2022-02-13 20:32:02 +0100

    Authentiseren token ingeregeld in Command.

--- symfony/src/Command/ImportGuidelinesCommand.php
+++ symfony/src/Command/ImportGuidelinesCommand.php
@@ -3,6 +3,7 @@
 // src/Command/ImportGuidelinesCommand.php
 namespace App\Command;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,14 +18,16 @@ use Symfony\Component\HttpClient\HttpClient;
 class ImportGuidelinesCommand extends Command
 {
     protected static $defaultName = 'app:import-guidelines';
+
     protected static $defaultDescription = 'Test';
 
-    public function __construct(bool $test = false)
+    public function __construct(bool $test = false, ManagerRegistry $doctrine)
     {
-        // best practices recommend to call the parent constructor first and
-        // then set your own properties. That wouldn't work in this case
-        // because configure() needs the properties set in this constructor
-        $this->test = $test;
+        $this->doctrine = $doctrine;
+        $this->entityManager = $doctrine->getManager();
+        $this->apiUrl = 'https://rdbapi.staging.sidekickit.nl';
+
+        $this->authenticate();
 
         parent::__construct();
     }
@@ -34,31 +37,23 @@ class ImportGuidelinesCommand extends Command
         //
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        // Access token request
+    private function authenticate(): void {
         $client = HttpClient::create();
-        $response = $client->request('POST', 'https://rdbauth.staging.sidekickit.nl/token',
+
+        $tokenResponse = $client->request('POST', 'https://rdbauth.staging.sidekickit.nl/token',
             [
                 'body' => [
                     'grant_type' => 'client_credentials',
                     'client_id' => 'f8ceed63597fd9a05b942bbb58360430',
                     'client_secret' => '402ebc5ce2b94fd803518d97364b5d0ceecf291975f5c265b6fd69f7e974c85b62e6dcd770c8814a18c9b71e8a064678313d74206d3c9118be8f09c454b2bb5d'
                 ],
-            ]);
+            ])->toArray();
 
-        $responseArray = $response->toArray();
+        $this->authHeaders = ['Authorization' =>  $tokenResponse['token_type'] . ' ' . $tokenResponse['access_token']];
+    }
 
-        // Rest API
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'https://rdbapi.staging.sidekickit.nl/guidelines',
-            [
-                'headers' => [
-                    'Authorization' =>  $responseArray['token_type'] . ' ' . $responseArray['access_token'],
-                ],
-            ]);
-        dump($response->toArray()); die;
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
 
-        return Command::SUCCESS;
     }
 }
